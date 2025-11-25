a92aced67bbe93dc44a35e893b7b2c0b0e9e6619
Takumi Hirota <hirota.takumi.uitec@gmail.com>
2025-11-25 16:36:24 +0900

    esp32_mqtt.inoの編集

--- esp32_mqtt/esp32_mqtt.ino
+++ esp32_mqtt/esp32_mqtt.ino
@@ -10,17 +10,17 @@
 #include <WiFiUdp.h>
 
 /* -------------------------定義分------------------------- */
-/* wifi config */
-/* 接続先wifiのSSIDとPASSを設定 */
-#define WIFI_SSID "ssid"
-#define WIFI_PASSWORD "password"
-
-/* MQTT config */
-#define MQTT_SERVER "MQTTブローカのIPアドレス"  //例:xx.xx.xx.xx
-#define MQTT_PORT 1883
-#define MQTT_BUFFER_SIZE 256
-#define TOPIC "esp32/bme/00"
-#define DEVICE_ID "esp001"  //デバイスIDは機器ごとにユニーク
+/* wifi config */ 
+/* 接続先wifiのSSIDとPASSを設定 */ 
+#define WIFI_SSID "ssid" 
+#define WIFI_PASSWORD "password" 
+ 
+/* MQTT config */ 
+#define MQTT_SERVER "MQTTブローカのIPアドレス"  //例:xx.xx.xx.xx 
+#define MQTT_PORT 1883 
+#define MQTT_BUFFER_SIZE 256 
+#define TOPIC "esp32/bme/00" 
+#define DEVICE_ID "esp000"   //デバイスIDは機器ごとにユニーク
 
 /* PIN config */
 #define SW1 25
@@ -42,30 +42,36 @@ LCD_ST7032 lcd;
 Ticker tickerMeasure;
 
 /* MQTT用インスタンス作成 */
-//WiFiClientのクラスからこのプログラムで実際に利用するWiFiClientのオブジェクトをespClientとして作成
+// WiFiClientのクラスからこのプログラムで実際に利用するWiFiClientのオブジェクトをespClientとして作成
 WiFiClient espClient;
-//Clientからブローカへの通信を行うPublish、ブローカへデータの受信を要求するSubscribeの処理などの、MQTTの通信を行うためのPubsubClientのクラスから実際に処理を行うオブジェクトclientを作成
+// Clientからブローカへの通信を行うPublish、ブローカへデータの受信を要求するSubscribeの処理などの、MQTTの通信を行うためのPubsubClientのクラスから実際に処理を行うオブジェクトclientを作成
 PubSubClient client(espClient);
 
+/* IP config の設定 */ 
+IPAddress local_ip(XX, XX, XX, XX); 
+IPAddress subnet(YY, YY, YY, YY); 
+IPAddress gateway(ZZ, ZZ, ZZ, ZZ);
+
 /* MQTT Publish用変数 */
-//JSONのオブジェクトを時間、温度、湿度、気圧用に4つの項目のため作成
+// JSONのオブジェクトを時間、温度、湿度、気圧用に4つの項目のため作成
 const int message_capacity = JSON_OBJECT_SIZE(4);
-//静的にJSONデータを生成するためにメモリを確保
+// 静的にJSONデータを生成するためにメモリを確保
 StaticJsonDocument<message_capacity> json_message;
-//JSONデータを格納する文字型配列のサイズを256に設定
+// JSONデータを格納する文字型配列のサイズを256に設定
 char message_buffer[MQTT_BUFFER_SIZE];
 
 /* NTPサーバ用インスタンス作成 */
 WiFiUDP ntpUDP;                // UDP client
 NTPClient timeClient(ntpUDP);  // NTP client
 
-//表示モード用変数
+// 表示モード用変数
 unsigned int mode = 0;
 
 /* ------------------------------各種関数定義------------------------ */
 /* WiFiの設定及び接続 */
 void WiFi_init(void) {
-  //connect wifi
+  // connect wifi
+  WiFi.config(local_ip, gateway, subnet);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println(".");
@@ -75,7 +81,7 @@ void WiFi_init(void) {
   Serial.println("");
   Serial.print("Connected : ");
   Serial.println(WiFi.localIP());
-  //sync Time
+  // sync Time
   configTime(3600L * 9, 0, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
 }
 
@@ -84,7 +90,7 @@ void Mqtt_connect(void) {
   //サーバーへの接続を維持できるように、これを定期的に呼び出す必要がある
   client.loop();
 
-  //MQTT未接続の場合は，再接続
+  // MQTT未接続の場合は，再接続
   while (!client.connected()) {
     Serial.println("Mqtt Reconnecting");
     if (client.connect(DEVICE_ID)) {
@@ -99,7 +105,7 @@ void PublishSensorData(void) {
   //センサからデータの取得
   bme.readAllMeasurements(&measurements);
 
-  //シリアルモニタに取得時間とセンサデータを表示
+  // シリアルモニタにセンサデータを表示
   Serial.println("Timestamp");
   Serial.println(timeClient.getFormattedTime());
   Serial.println("Humidity,Pressure,BME-Temp");
@@ -110,23 +116,22 @@ void PublishSensorData(void) {
   Serial.println(measurements.temperature, 2);
 
   /* ペイロードを作成して送信を行う．*/
-  //JSONデータをクリア
+  // JSONデータをクリア
   json_message.clear();
 
-  //JSONの項目をキーと値を添えてJSONを作成
+  // JSONの項目をキーと値を添えてJSONを作成
   json_message["timestamp"] = timeClient.getFormattedTime();
   json_message["humid"] = measurements.humidity;
   json_message["press"] = measurements.pressure / 100;
   json_message["temp"] = measurements.temperature;
 
-  //json_messageの中のJSONデータをJSON形式の文字列message_bufferとしてシリアライズ化（文字列に変換）
+  // json_messageの中のJSONデータをJSON形式の文字列message_bufferとしてシリアライズ化（文字列に変換）
   serializeJson(json_message, message_buffer, sizeof(message_buffer));
 
-  //トピックをesp32/bmeして、JSON形式の文字列をパブリッシュする
+  // トピックをesp32/bmeして、JSON形式の文字列をパブリッシュする
   client.publish(TOPIC, message_buffer);
 }
 
-/* スイッチの状態確認 */
 void Switch_check(void) {
   if (!digitalRead(SW1)) {
     lcd.clear();
@@ -151,7 +156,7 @@ void Switch_check(void) {
   }
 }
 
-/* setup関数 */
+
 void setup() {
   Serial.begin(115200);
 
@@ -164,24 +169,24 @@ void setup() {
       ;  //Freeze
   }
 
-  //WiFi接続
+  // WiFi接続
   WiFi_init();
 
-  //インスタント化したオブジェクトclientの接続先のサーバを、アドレスとポート番号を設定
+  // インスタント化したオブジェクトclientの接続先のサーバを、アドレスとポート番号を設定
   client.setServer(MQTT_SERVER, MQTT_PORT);
 
-  //5secごとにセンサデータを取得及びMQTTBrokerへPublish
+  // 5secごとにセンサデータを取得及びMQTTBrokerへPublish
   tickerMeasure.attach_ms(5000, PublishSensorData);
 
-  //ST7032設定
+  // ST7032設定
   lcd.begin();
   lcd.setcontrast(20);
 
   //ntp設定
-  timeClient.begin();               //init NTP
-  timeClient.setTimeOffset(32400);  //0= GMT, 3600 = GMT+1, 32400 = GMT+9
+  timeClient.begin();               // init NTP
+  timeClient.setTimeOffset(32400);  // 0= GMT, 3600 = GMT+1, 32400 = GMT+9
 
-  //PIN設定
+  // PIN設定
   pinMode(SW1, INPUT_PULLUP);
   pinMode(SW2, INPUT_PULLUP);
   pinMode(SW3, INPUT_PULLUP);
@@ -190,10 +195,12 @@ void setup() {
   pinMode(Y_LED, OUTPUT);
 }
 
-/* loop関数 */
 void loop() {
-  Mqtt_connect();       //MQTTBrokerへの接続
-  Switch_check();       //タクトSWの状態読取り
+
+  Mqtt_connect();  // MQTTBrokerへの接続
+
+  Switch_check();  // タクトSWの状態読取り
+
   timeClient.update();  //ntp更新
 
   switch (mode) {
